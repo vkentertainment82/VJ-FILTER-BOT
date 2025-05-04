91659eb48862b7030d2cdb634c2255324ae5a66e
vkentertainment82 <vkentertainment2024@gmail.com>
2025-05-04 10:57:26 +0530

    Update info.py

--- info.py
+++ info.py
@@ -112,7 +112,7 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 
 # Token Verification Info :
-VERIFY = bool(environ.get('VERIFY', True))
+VERIFY = bool(environ.get('VERIFY', False))
 VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'http://Shortxlinks.com')
 VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', '1c34885247cd49c5c471cc61ea5438d8dd6fdc08')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', '')
